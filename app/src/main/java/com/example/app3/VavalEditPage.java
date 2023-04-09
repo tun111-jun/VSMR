fe09fbb32843b87319394463c41b685ce7d0402e
GA-EUN-KIM <dkrk333@naver.com>
2023-04-09 23:37:12 +0900

    VavalEditPage추가, playVideo 조건문 수정

---
+++ app/src/main/java/com/example/app3/VavalEditPage.java
@@ -0,0 +1,14 @@
+package com.example.app3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VavalEditPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vaval_edit_page);
+    }
+}
